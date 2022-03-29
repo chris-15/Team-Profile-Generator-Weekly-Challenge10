a9a1f0c6670ecc6b98959ebd3ff3d6f10ce6bf4b
chris-15 <chrismac@Chriss-MacBook-Pro.local>
2022-03-29 00:53:23 -0400

    created getGitHub and getRole for engineer and passed tests

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -2,7 +2,7 @@ const { expect, test } = require('@jest/globals');
 const Engineer = require('../lib/Engineer');
 
 test("creates an engineer object", () => {
-    const engineer = new Engineer('Chris', 15, 'christopher.sarm15@gmail.com', 'chris15');
+    const engineer = new Engineer('Chris', 15, 'christopher.sarm15@gmail.com', 'chris-15');
 
     expect(engineer.name).toEqual(expect.any(String));
     expect(engineer.id).toEqual(expect.any(Number));
@@ -11,13 +11,13 @@ test("creates an engineer object", () => {
 })
 
 test("creates method to return engineer's github", () => {
-    const engineer = new Engineer('Chris', 15, 'christopher.sarm15@gmail.com', 'chris15');
+    const engineer = new Engineer('Chris', 15, 'christopher.sarm15@gmail.com', 'chris-15');
 
     expect(engineer.getGitHub()).toEqual(expect.any(String));
 })
 
 test("creates method to return engineer role", () => {
-    const engineer = new Engineer('Chris', 15, 'christopher.sarm15@gmail.com', 'chris15');
+    const engineer = new Engineer('Chris', 15, 'christopher.sarm15@gmail.com', 'chris-15');
 
     expect(engineer.getRole()).toEqual("Engineer");
 })
