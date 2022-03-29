37377c31d0570aff13ed04518f861166139099ea
chris-15 <chrismac@Chriss-MacBook-Pro.local>
2022-03-29 00:50:21 -0400

    created failing test for engineer role and get github

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,3 +1,4 @@
+const { expect, test } = require('@jest/globals');
 const Engineer = require('../lib/Engineer');
 
 test("creates an engineer object", () => {
@@ -7,4 +8,16 @@ test("creates an engineer object", () => {
     expect(engineer.id).toEqual(expect.any(Number));
     expect(engineer.email).toEqual(expect.any(String));
     expect(engineer.github).toEqual(expect.any(String));
+})
+
+test("creates method to return engineer's github", () => {
+    const engineer = new Engineer('Chris', 15, 'christopher.sarm15@gmail.com', 'chris15');
+
+    expect(engineer.getGitHub()).toEqual(expect.any(String));
+})
+
+test("creates method to return engineer role", () => {
+    const engineer = new Engineer('Chris', 15, 'christopher.sarm15@gmail.com', 'chris15');
+
+    expect(engineer.getRole()).toEqual("Engineer");
 })
