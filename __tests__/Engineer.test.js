2c18b2ff906ec3bb266565b714a17ddd41d7eefd
chris-15 <chrismac@Chriss-MacBook-Pro.local>
2022-03-29 20:24:58 -0400

    rewrote failing engineer test to be more precise

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,18 +1,26 @@
-const { expect, test } = require('@jest/globals');
 const Engineer = require('../lib/Engineer');
 
 test("creates an engineer object", () => {
     const engineer = new Engineer('Chris', 15, 'christopher.sarm15@gmail.com', 'chris-15');
-
+    
+    expect(engineer.name).toEqual('Chris');
     expect(engineer.name).toEqual(expect.any(String));
+
+    expect(engineer.id).toEqual(15);
     expect(engineer.id).toEqual(expect.any(Number));
+
+    expect(engineer.email).toEqual('christopher.sarm15@gmail.com');
     expect(engineer.email).toEqual(expect.any(String));
+
+    expect(engineer.github).toEqual('chris-15');
     expect(engineer.github).toEqual(expect.any(String));
+
 })
 
 test("creates method to return engineer's github", () => {
     const engineer = new Engineer('Chris', 15, 'christopher.sarm15@gmail.com', 'chris-15');
-
+    
+    expect(engineer.getGitHub()).toEqual('chris-15');
     expect(engineer.getGitHub()).toEqual(expect.any(String));
 })
 
