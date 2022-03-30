dd2da6ac5bd4df624c31a467df98ca9abf806833
chris-15 <chrismac@Chriss-MacBook-Pro.local>
2022-03-29 20:13:13 -0400

    rewrote failing manager test to be more precise

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -3,9 +3,16 @@ const Manager = require('../lib/Manager.js');
 test("creates a manager object", () => {
     const manager = new Manager('Chris', 15, 'christopher.sarm15@gmail.com', 2);
 
+    expect(manager.name).toEqual('Chris');
     expect(manager.name).toEqual(expect.any(String));
+
+    expect(manager.id).toEqual(15);
     expect(manager.id).toEqual(expect.any(Number));
+
+    expect(manager.email).toEqual('christopher.sarm15@gmail.com')
     expect(manager.email).toEqual(expect.any(String));
+
+    expect(manager.officeNumber).toEqual(2);
     expect(manager.officeNumber).toEqual(expect.any(Number));
 
 })
